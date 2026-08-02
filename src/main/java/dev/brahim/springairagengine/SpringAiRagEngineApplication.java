db84fc216f30ec85e2cdacf51c3eaf77aeae31f9
Brahim Naji <ba3105947@gmail.com>
2026-08-02 18:11:24 +0100

    Initial commit

---
+++ src/main/java/dev/brahim/springairagengine/SpringAiRagEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.brahim.springairagengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiRagEngineApplication.class, args);
+	}
+
+}
