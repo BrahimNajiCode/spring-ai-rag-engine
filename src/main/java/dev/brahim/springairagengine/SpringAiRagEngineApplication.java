53a992c2f79ae59f91868062fd560ee5ec10516c
Brahim Naji <ba3105947@gmail.com>
2026-08-03 19:41:25 +0100

    style: reformat application entry point and test to 4-space indent

--- src/main/java/dev/brahim/springairagengine/SpringAiRagEngineApplication.java
+++ src/main/java/dev/brahim/springairagengine/SpringAiRagEngineApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAiRagEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAiRagEngineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiRagEngineApplication.class, args);
+    }
 
 }
