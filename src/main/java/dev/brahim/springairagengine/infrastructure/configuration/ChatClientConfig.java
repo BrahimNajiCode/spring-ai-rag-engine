5e474529ab47b0decf92798feed38fe98d090d59
Brahim Naji <ba3105947@gmail.com>
2026-08-08 11:00:07 +0100

    feat: add ChatClient bean configuration

---
+++ src/main/java/dev/brahim/springairagengine/infrastructure/configuration/ChatClientConfig.java
@@ -0,0 +1,16 @@
+package dev.brahim.springairagengine.infrastructure.configuration;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder){
+        return builder.build();
+    }
+
+}
